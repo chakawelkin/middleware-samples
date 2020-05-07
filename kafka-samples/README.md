eee185d155b3ca5ff3541997838e8a53b03518b1
chaka.hu <chaka.hu@ximalaya.com>
2020-05-07 17:58:42 +0800

    kafka 示例

---
+++ kafka-samples/README.md
@@ -0,0 +1,4 @@
+kafka 连接原理
+首先连接 192.168.0.141:9092
+再连接返回的host.name = bogon,
+最后继续连接advertised.host.name=bogon
---
+++ kafka-samples/src/main/java/com/chaka/middleware/kafka/ConsumerFastStart.java
@@ -0,0 +1,40 @@
+package com.chaka.middleware.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerFastStart {
+
+    public static final String brokerList = "114.67.170.152:9092";
+    public static final String topic = "topic-demo";
+    public static final String groupId = "group.demo";
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("bootstrap.servers", brokerList);
+        //设置消费组的名称，具体的释义可以参见第3章
+        properties.put("group.id", groupId);
+        //创建一个消费者客户端实例
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        //订阅主题
+        consumer.subscribe(Collections.singletonList(topic));
+        //循环消费消息
+        while (true) {
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("消息为:" + record.value());
+            }
+        }
+    }
+
+}
